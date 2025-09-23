97a9ba55a559f687cfb836543b8faa97e46e015a
iago.lfarias <iago.lfarias@CAM0710759W11-1>
2025-09-23 17:45:18 -0300

    Adicionar arquivos de projeto.

---
+++ Formulario1809/Form2.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Formulario1809
+{
+    partial class form_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_login));
+            lbl_email = new Label();
+            lbl_senha = new Label();
+            txt_user = new TextBox();
+            txt_senha = new TextBox();
+            btn_button = new Button();
+            btn_cadastro = new Button();
+            SuspendLayout();
+            // 
+            // lbl_email
+            // 
+            lbl_email.AutoSize = true;
+            lbl_email.BackColor = SystemColors.Control;
+            lbl_email.Location = new Point(262, 65);
+            lbl_email.Name = "lbl_email";
+            lbl_email.Size = new Size(47, 15);
+            lbl_email.TabIndex = 0;
+            lbl_email.Text = "Usuário";
+            // 
+            // lbl_senha
+            // 
+            lbl_senha.AutoSize = true;
+            lbl_senha.Location = new Point(262, 184);
+            lbl_senha.Name = "lbl_senha";
+            lbl_senha.Size = new Size(39, 15);
+            lbl_senha.TabIndex = 1;
+            lbl_senha.Text = "Senha";
+            // 
+            // txt_user
+            // 
+            txt_user.Location = new Point(262, 95);
+            txt_user.Name = "txt_user";
+            txt_user.Size = new Size(290, 23);
+            txt_user.TabIndex = 2;
+            // 
+            // txt_senha
+            // 
+            txt_senha.Location = new Point(262, 217);
+            txt_senha.Name = "txt_senha";
+            txt_senha.Size = new Size(290, 23);
+            txt_senha.TabIndex = 3;
+            txt_senha.UseWaitCursor = true;
+            // 
+            // btn_button
+            // 
+            btn_button.Location = new Point(361, 303);
+            btn_button.Name = "btn_button";
+            btn_button.Size = new Size(75, 23);
+            btn_button.TabIndex = 4;
+            btn_button.Text = "Entrar";
+            btn_button.UseVisualStyleBackColor = true;
+            btn_button.Click += btn_button_Click;
+            // 
+            // btn_cadastro
+            // 
+            btn_cadastro.Location = new Point(361, 354);
+            btn_cadastro.Name = "btn_cadastro";
+            btn_cadastro.Size = new Size(75, 23);
+            btn_cadastro.TabIndex = 5;
+            btn_cadastro.Text = "Cadastrar";
+            btn_cadastro.UseVisualStyleBackColor = true;
+            btn_cadastro.Click += btn_cadastro_Click;
+            // 
+            // form_login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btn_cadastro);
+            Controls.Add(btn_button);
+            Controls.Add(txt_senha);
+            Controls.Add(txt_user);
+            Controls.Add(lbl_senha);
+            Controls.Add(lbl_email);
+            MaximizeBox = false;
+            Name = "form_login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            Load += form_login_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbl_email;
+        private Label lbl_senha;
+        private TextBox txt_user;
+        private TextBox txt_senha;
+        private Button btn_button;
+        private Button btn_cadastro;
+    }
+}
---
+++ Formulario1809/Form2.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario1809
+{
+    public partial class form_login : Form
+    {
+        string dbPath = @"C:\Users\iago.lfarias\Downloads\Banco\Contato.db";
+
+        string connectionString;
+        public form_login()
+        {
+            InitializeComponent();
+
+            connectionString = $"Data Source={dbPath};Version=3;";
+        }
+
+        private void form_login_Load(object sender, EventArgs e)
+        {
+            conexao();
+        }
+
+        private void conexao()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    MessageBox.Show("RODO!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"BURRO NÃO RODO {ex.Message}");
+            }
+        }
+
+        private void btn_button_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txt_user.Text))
+            {
+                MessageBox.Show("O campo Usuário é obrigatório!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txt_user.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txt_senha.Text))
+            {
+                MessageBox.Show("O campo Senha é obrigatório!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txt_user.Focus();
+                return;
+            }
+            else
+            {
+                validar();
+            }
+
+        }
+
+        private void validar()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "SELECT * FROM Usuarios WHERE NomeUsuario = @NomeUsuario AND Senha = @Senha";
+
+                    using (var cmd = new SQLiteCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@NomeUsuario", txt_user.Text);
+
+                        cmd.Parameters.AddWithValue("@Senha", txt_senha.Text);
+
+                        cmd.ExecuteNonQuery();
+
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                Form1 form = new Form1();
+                                form.Show();
+                                this.Hide();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Usuário ou Senha incorretos!");
+                            }
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao efetuar Login: {ex.Message}");
+            }
+        }
+
+        private void btn_cadastro_Click(object sender, EventArgs e)
+        {
+            form_cadastro form = new form_cadastro();
+            form.Show();
+            this.Hide();
+        }
+    }
+}
